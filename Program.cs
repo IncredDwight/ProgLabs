802a90aa6f20248adde8a6953e61503bcd957387
Yaroslav_Valchyshen <51487067+IncredDwight@users.noreply.github.com>
2021-10-07 20:16:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            Console.Write("Enter x: ");
+            x = Double.Parse(Console.ReadLine());
+            Console.Write("Enter y: ");
+            y = Double.Parse(Console.ReadLine());
+            if (Math.Abs(y) >= Math.Pow(x, 2) && Math.Abs(x) >= Math.Pow(y, 2))
+                Console.WriteLine("Point is inside plane");
+            else
+                Console.WriteLine("Point is outside plane");
+        }
+    }
+}
