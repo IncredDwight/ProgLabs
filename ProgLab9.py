8c7775019178408d151c42526e41477a868354f7
Yaroslav_Valchyshen <51487067+IncredDwight@users.noreply.github.com>
2021-12-25 17:44:12 +0200

    Delete ProgLab9.py

--- ProgLab9.py
+++
@@ -1,8 +0,0 @@
-line = input("Enter line: ")
-wordLength = int(input("Enter word's length: "))
-wordsArray = line.split(" ")
-i = 0
-while(i < len(wordsArray)):
-    if(len(wordsArray[i]) == wordLength):
-        print(wordsArray[i] + " ")
-    i+=1
